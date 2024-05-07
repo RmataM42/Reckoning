0872f0b2ae6f66bea209340b7775dc66f22c6ae0
RmataM7 <rmuadthong@gmail.com>
2024-05-07 15:27:45 -0700

    new weekly and DB dump

--- main.py
+++ main.py
@@ -9,21 +9,21 @@ client = MongoClient("mongodb://localhost:27017/")
 
 # File paths
 file_paths = [
-    "weekly_csv/week_4.csv",
-    "weekly_csv/week_5.csv",
-    "weekly_csv/week_6.csv",
-    "weekly_csv/week_7.csv",
-    "EG4-DBDump (1).xlsx"
+    "/Users/amatamuadthong/Desktop/467_multi_media/Project/Reckoning/weekly_csv/week_4.csv",
+    "/Users/amatamuadthong/Desktop/467_multi_media/Project/Reckoning/weekly_csv/week_5.csv",
+    "/Users/amatamuadthong/Desktop/467_multi_media/Project/Reckoning/weekly_csv/week_6.csv",
+    "/Users/amatamuadthong/Desktop/467_multi_media/Project/Reckoning/weekly_csv/week_7.csv",
+    "EG4-DBDump.csv"
 ]
 
 # Read each CSV file into a DataFrame and print it
 for file_path in file_paths:
     print(f"File: {file_path}")
-    try:
-        df = pd.read_csv(file_path, encoding='ISO-8859-1')
-        print(df.to_string())
-        print("\n")
-    except FileNotFoundError:
-        print(f"Error: File {file_path} not found.\n")
-    except Exception as e:
-        print(f"Error reading file {file_path}: {e}\n")
+    # try:
+    #     df = pd.read_csv(file_path, encoding='ISO-8859-1')
+    #     print(df.to_string())
+    #     print("\n")
+    # except FileNotFoundError:
+    #     print(f"Error: File {file_path} not found.\n")
+    # except Exception as e:
+    #     print(f"Error reading file {file_path}: {e}\n")
