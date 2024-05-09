d2f20707b70a6cf92d04a0b610cc1371eb75d7d7
RmataM7 <rmuadthong@gmail.com>
2024-05-08 20:00:08 -0700

    console output repeatable

--- main.py
+++ main.py
@@ -72,7 +72,30 @@ def print_test_case():
     print("\nTotal: ", count)
 
 def print_repeatable():
-    print('j')
+    query1 = {"Repeatable?": {"$regex": "^\s*Yes\s*$", "$options": "i"}}
+    # "$regex": "^\s*Yes\s*$" means the value starts (^) and ends ($) with "Yes",
+    # and there can be any amount of whitespace (\s*) before and after "Yes"
+    # "$options": "i" makes the regex case-insensitive
+    query2 = {
+        "Repeatable?": {"$regex": "^\s*yes\s*$", "$options": "i"},
+        "Test Owner": {"$not": {"$eq": "Rmata Muadthong"}}
+    }
+    count1 = 0
+    count2 = 0
+    for collection1_loop in [collection1]:
+        repeatable = collection1_loop.find(query1)
+        for x in repeatable:
+            print(x)
+            count1 += 1
+    print("\n-----------------------\n")
+    for collection2_loop in [collection2]:
+        repeatable = collection2_loop.find(query1)
+        for x in repeatable:
+            print(x)
+            count2 += 1
+    total_count = count1 + count2
+    print(f"\nTotal repeatable count: {total_count}\nRepeatable collection 1: {count1}\nRepeatable collection 2: {count2}")
+
 
 
 # -------------------------------------
